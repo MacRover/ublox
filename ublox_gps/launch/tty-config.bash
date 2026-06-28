129d7ddc9ec0923ea7b851714227159be9d0a1a7
chosterto <johnnychox@gmail.com>
2026-06-28 16:38:10 -0400

    chmod scripts for rover ports - zach

---
+++ ublox_gps/launch/tty-config.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo chmod 666 /dev/ttyUSB0
+sudo chmod 666 /dev/ttyACM0
+echo Yay it worked
---
+++ ublox_gps/launch/tty-setup.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo tee /etc/udev/rules.d/usb-serial.rules > /dev/null <<EOF
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", MODE="0666"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="1546", ATTRS{idProduct}=="01a9", MODE="0666"
+EOF
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+echo it worked
