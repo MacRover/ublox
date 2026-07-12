92a7af6b763e1667764bcea9127bcb944743a797
chosterto <johnnychox@gmail.com>
2026-07-12 12:35:03 -0400

    added symlinks to chmod script - zach

--- ublox_gps/launch/tty-setup.bash
+++ ublox_gps/launch/tty-setup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo tee /etc/udev/rules.d/usb-serial.rules > /dev/null <<EOF
-SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", MODE="0666"
-SUBSYSTEM=="tty", ATTRS{idVendor}=="1546", ATTRS{idProduct}=="01a9", MODE="0666"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", MODE="0666", SYMLINK+="ttyMovingBase"
+SUBSYSTEM=="tty", ATTRS{idVendor}=="1546", ATTRS{idProduct}=="01a9", MODE="0666", SYMLINK+="ttyRover"
 EOF
 
 sudo udevadm control --reload-rules
